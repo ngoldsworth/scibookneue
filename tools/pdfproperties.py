c6158539695eb9b5b9ac8c4eee81aaf8019d6400
Nelson Goldsworth <nelson.goldsworth@gmail.com>
2019-10-19 13:17:55 -0600

    Adding tool for PDF sizes. Updating README
    Since the goal of this project is to eventually create a LaTeX class
    that provides both print formats and screen formats, its probably
    a good idea to examine various textbook page dimensions and look
    at whats most common.
    
    I am writing a python script to do that, but its giving me trouble

--- tools/pdfproperties.py
+++ tools/pdfproperties.py
@@ -1,4 +1,6 @@
 import pathlib as pl
+import re
+import subprocess
 from PyPDF2 import PdfFileReader
 
 
@@ -10,21 +12,41 @@ def file_list(extension: list or str, directory: pl.Path):
     # extension = [pl.Path(e) for e in extension]
 
     filelist = []
-    for type in extension:
-        wildcardformat = '**/*.' + type
+    for ftype in extension:
+        wildcardformat = '**/*.' + ftype
         filelist.extend(sorted(directory.glob(wildcardformat)))
 
     return sorted(filelist)
 
+
 def get_pdf_dimensions(file: pl.Path):
-f = PdfFileReader(open(str(file), 'rb'))
-    dim = f.getPage(1).mediaBox
-    return dim
+    result = subprocess.run(['pdfinfo', str(file)], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    matches = re.search(r'Page size:\s+([\d\.]+)\sx\s([\d\.]+)', result)
+    if not isinstance(matches, type(None)):
+        return float(matches.group(1)), float(matches.group(2))
+    else:
+        return None
+
 
 if __name__ =='__main__':
+    import numpy as np
+    import matplotlib.pyplot as plt
     pdffolder = pl.Path('/media/nelson/warhol/books/')
     fl = file_list('pdf', pdffolder)
+    outof = len(fl)
+    tupslist = []
+    count = 0
     for f in fl:
-        print(get_pdf_dimensions(f))
+        dim = get_pdf_dimensions(f)
+        count += 1
+        if dim != None:
+            tupslist.append(dim)
+
+        if count % 20 == 0:
+            print(fl.index(f)/outof)
+
+    sizes = np.asarray(tupslist)
+    plt.scatter(sizes[1:], sizes[0:])
+    plt.show()
 
 
